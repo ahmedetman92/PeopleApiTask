e13e9974b8295f6f0d01bd238b9695946db9e3b8
dinaelhakim <dinaelhakim135@gmail.com>
2018-05-04 20:21:48 +0200

    By Ahmed Etman: [+] adding offline mode

--- app/src/main/java/com/example/ahmedetman/peopleapitask/models/CharactersListDataProvider.java
+++ app/src/main/java/com/example/ahmedetman/peopleapitask/models/CharactersListDataProvider.java
@@ -6,6 +6,7 @@ import com.example.ahmedetman.peopleapitask.models.caching.DBHelper;
 import com.example.ahmedetman.peopleapitask.network.ApiService;
 import com.example.ahmedetman.peopleapitask.network.RetroClient;
 import com.example.ahmedetman.peopleapitask.presenters.MainActivityPresenter;
+import com.example.ahmedetman.peopleapitask.utils.NetworkConnectionUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,40 +38,53 @@ public class CharactersListDataProvider {
      */
     public void getAllCharacters(final MainActivityPresenter mainActivityPresenter) {
 
-        DBHelper dbHelper = new DBHelper(ApplicationContextProvider.getContext());
+        //checking the network availability, if there is no network. Then use offline mode.
+        if (NetworkConnectionUtil.isNetworkAvailable()) {
+            //Creating an object of our api interface
+            ApiService api = RetroClient.getApiService();
 
-        List<CharacterItem> characterItems = dbHelper.getAll(CharacterItem.class);
+            /**
+             * Calling JSON
+             */
+            Call<CharactersList> call = api.getCharactersCall();
 
-        //Creating an object of our api interface
-        ApiService api = RetroClient.getApiService();
+            /**
+             * Enqueue Callback will be call when get response...
+             */
+            call.enqueue(new Callback<CharactersList>() {
+                @Override
+                public void onResponse(Call<CharactersList> call, Response<CharactersList> response) {
 
-        /**
-         * Calling JSON
-         */
-        Call<CharactersList> call = api.getCharactersCall();
-
-        /**
-         * Enqueue Callback will be call when get response...
-         */
-        call.enqueue(new Callback<CharactersList>() {
-            @Override
-            public void onResponse(Call<CharactersList> call, Response<CharactersList> response) {
-
-                if (response.isSuccessful()) {
-                    if (response.body() != null && response.body().getResults() != null) {
-                        mainActivityPresenter.onLoadCharactersSuccess(response.body().getResults());
-                        mainActivityPresenter.cacheCharactersList(response.body().getResults());
-                    }else {
-                        mainActivityPresenter.onLoadCharactersFail(ApplicationContextProvider
-                                .getContext().getString(R.string.error_empty_body));
+                    if (response.isSuccessful()) {
+                        if (response.body() != null && response.body().getResults() != null) {
+                            mainActivityPresenter.onLoadCharactersSuccess(response.body().getResults());
+                            mainActivityPresenter.cacheCharactersList(response.body().getResults());
+                        } else {
+                            mainActivityPresenter.onLoadCharactersFail(ApplicationContextProvider
+                                    .getContext().getString(R.string.error_empty_body));
+                        }
                     }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<CharactersList> call, Throwable t) {
-                mainActivityPresenter.onLoadCharactersFail(t.getMessage());
-            }
-        });
+                @Override
+                public void onFailure(Call<CharactersList> call, Throwable t) {
+                    mainActivityPresenter.onLoadCharactersFail(t.getMessage());
+                    getDataForOfflineMode(mainActivityPresenter);
+                }
+            });
+        } else {
+            getDataForOfflineMode(mainActivityPresenter);
+        }
+    }
+
+    private void getDataForOfflineMode(MainActivityPresenter mainActivityPresenter) {
+        DBHelper dbHelper = new DBHelper(ApplicationContextProvider.getContext());
+        List<CharacterItem> characterItems = dbHelper.getAll(CharacterItem.class);
+        if (characterItems != null) {
+            mainActivityPresenter.onLoadCharactersSuccess(characterItems);
+        } else {
+            mainActivityPresenter.onLoadCharactersFail(ApplicationContextProvider
+                    .getContext().getString(R.string.error_empty_body));
+        }
     }
 }
